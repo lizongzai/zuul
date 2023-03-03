3bab04473dc834fab6589ac7baaf9dfea91eafb2
root <lizongzai@gmail.com>
2023-03-03 14:19:28 +0800

    V1 准备高可用Eureka服务注册中心环境

--- eureka-server02/src/main/java/com/example/EurekaServer02Application.java
+++ eureka-server02/src/main/java/com/example/EurekaServer02Application.java
@@ -1,2 +1,16 @@
-package com.example;public class EurekaServer02Application {
+package com.example;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServer02Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EurekaServer02Application.class, args);
+  }
+
 }
