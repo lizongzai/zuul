2f8e4fc910d91090c4f2bee86af0856491a16588
root <lizongzai@gmail.com>
2023-03-04 15:26:32 +0800

    V5 自定义zuul服务网关过滤器

---
+++ zuul-server/src/main/java/com/example/filter/CustomZuulFilter.java
@@ -0,0 +1,68 @@
+package com.example.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import javax.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * 功能描述: 网关过滤器
+ *
+ * @author lizongzai
+ * @date 2023/03/04 15:04
+ * @since 1.0.0
+ */
+@Component
+public class CustomZuulFilter extends ZuulFilter {
+
+  private static final Logger logger = LoggerFactory.getLogger(CustomZuulFilter.class);
+
+  /**
+   * 过滤器类型 pre routing post error
+   *
+   * @return
+   */
+  @Override
+  public String filterType() {
+    return "pre";
+  }
+
+  /**
+   * 执行顺序 数值越小，优先级越高
+   *
+   * @return
+   */
+  @Override
+  public int filterOrder() {
+    return 0;
+  }
+
+  /**
+   * 执行条件 true 开启 false 关闭
+   *
+   * @return
+   */
+  @Override
+  public boolean shouldFilter() {
+    return true;
+  }
+
+  /**
+   * 动作（具体操作） 具体逻辑
+   *
+   * @return
+   * @throws ZuulException
+   */
+  @Override
+  public Object run() throws ZuulException {
+    // 获取请求上下文
+    RequestContext currentContext = RequestContext.getCurrentContext();
+    HttpServletRequest request = currentContext.getRequest();
+    logger.info("CustomZuulFilter--> method={}, url={}", request.getMethod(),
+        request.getRequestURL().toString());
+    return null;
+  }
+}
