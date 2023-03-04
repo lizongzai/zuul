4f7e4f943719c4bd3133206e3c9276f3585fe2ef
root <lizongzai@gmail.com>
2023-03-04 13:10:53 +0800

    V3 zuul服务网关

--- zuul-server/src/main/java/com/example/ZuulServerApplication.java
+++ zuul-server/src/main/java/com/example/ZuulServerApplication.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 
 @SpringBootApplication
-//@EnableZuulServer // 开启 Zuul 注解
 @EnableZuulProxy
 public class ZuulServerApplication {
 
